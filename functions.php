4534da1d269ae2f3646b3b48ce583dbdd6650403
Mayur Tembhare <contactmayurtembhare@gmail.com>
2025-02-07 13:59:11 +0530

    Update the hiring section

--- functions.php
+++ functions.php
@@ -1452,7 +1452,7 @@ function companies_hiring_shortcode($atts) {
                 $image_url = $atts['image' . $i];
                 if ($image_url) {
                     ?>
-                    <div class="col-2">
+                    <div class="col-lg-2 col-6">
                         <img src="<?php echo esc_url($image_url); ?>" alt="Company Logo <?php echo $i; ?>" class="company-logo" />
                     </div>
                     <?php
