27e15e1e851502927db5566b80c98f2378d68bd4
Mayur Tembhare <contactmayurtembhare@gmail.com>
2025-02-07 15:52:13 +0530

    Testing tools

--- functions.php
+++ functions.php
@@ -1308,6 +1308,7 @@ function tools_covered_shortcode($atts) {
         <p class="text-large-normal"><em><?php echo $caption; ?></em></p>
         <h2 class="h2"><?php echo $heading; ?></h2>
         <div class="row text-center align-items-center">
+            <?php echo $tools; ?>
             <?php foreach ($tools as $index => $tool) : ?>
                 <div class="col-6 col-md-2 mb-3">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/<?php echo esc_attr($tool['image']); ?>"
