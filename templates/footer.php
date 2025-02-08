bed646af59d74d8d794bc00941fd39562f19e094
Mayur Tembhare <contactmayurtembhare@gmail.com>
2025-02-08 11:30:40 +0530

    TESting retrived

--- templates/footer.php
+++ templates/footer.php
@@ -151,6 +151,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
 <script src="https://owlcarousel2.github.io/OwlCarousel2/assets/vendors/highlight.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
         $('#courseslist .owl-carousel').owlCarousel({
